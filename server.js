acbe25948db5afcf9cfc6afa2ded87e4a0174ce6
Antonio FIlipov <tonkatafilipov@gmail.com>
2015-05-15 01:19:09 +0300

    add CRUD which manipulate and visualize json

--- server.js
+++ server.js
@@ -21,10 +21,20 @@ app.use(methodOverride());
 
 
 var orders = [
-
-    {name: 'Телешко', price: '12'},
-    {name: 'Пилеико', email: '13'},
+    // {client: 1, products: [{id:1,name:'Chicken', price:12}, {id:1,name:'Carrot', price:10}]},
+    // {client: 2, products: [{id:1,name:'Meat', price:13}]}
 ];
+
+function getIndexById(collection, id){
+    var docIndex = -1;
+    collection.forEach(function(item, index){
+        if(item["id"] === id){
+            docIndex = index;
+        }
+    })
+    return docIndex;
+}
+
 // routes
 app.get('/', function (req, res) {
   res.render('index');
@@ -37,18 +47,20 @@ app.get('/api/orders', function (req, res) {
 // post new user to the collection
 app.post('/api/orders', function (req, res) {
     // req.body contains the incoming fields and values
-    var id = req.body.id;
-    var name = req.body.name;
-    var price = req.body.price;
+    var client = req.body.client;
+    var products = req.body.products;
+
+
 // INDEX BY ID WEEK7
-    orders[id] = {
-        name: name,
-        price: price
+    order = {
+        client: 1,
+        products: products
     };
 
+    orders.push(order);
     res.jsonp({
         msg: 'order created',
-        data: orders[id]
+        data: order
     });
 });
 
@@ -58,14 +70,34 @@ app.post('/api/orders', function (req, res) {
 app.get('/api/orders/:id', function (req, res) {
     // get the id from the params
     var id = req.params.id;
-    res.jsonp(orders[id]);
+    var index = getIndexById(orders, id);
+
+    res.jsonp(orders[index]);
+});
+
+// put an updated version of a user by id
+app.put('/api/orders/:id', function(req, res){
+ // get the id from the params
+ var id = req.params.id;
+ var index = getIndexById(orders, id);
+
+ // update the info from the body if passed or use the existing one
+ users[index].name = req.body.name || users[index].name;
+ users[index].price = req.body.price || users[index].price;
+
+ res.jsonp({
+ msg: 'user data updated',
+ data: users[index]
+ });
 });
 
 // delete an existing user by id
 app.delete('/api/orders/:id', function (req, res) {
     var id = req.params.id;
-    if (orders[id]) {
-        delete(orders[id]);
+    var index = getIndexById(orders, id);
+
+    if (orders[index]) {
+        orders.splice(index,1);
         res.jsonp('order ' + id + ' successfully deleted!');
     } else {
         res.jsonp('order ' + id + ' does not exist!');
