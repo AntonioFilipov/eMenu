9a3a21994cdcfd4b08a81185934f5469e6548fe9
Antonio FIlipov <tonkatafilipov@gmail.com>
2015-05-15 01:26:41 +0300

    delete useless images from img folder

--- public/img/Hangman Game _ Codecademy_files/registrations_controller-b11f907dd50218e3893643c69ad41ce3.js
+++
@@ -1,2 +0,0 @@
-define("controllers/registrations_controller",["require","exports","module","libs/utils","jquery"],function(e,t){var n=e("libs/utils"),r=e("jquery");t.init=function(){var e=n.deparam(window.location.href).email;e&&r("#user_email").val(e)}})
-;
